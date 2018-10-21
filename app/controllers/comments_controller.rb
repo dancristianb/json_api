07b497337ad12835870583a5ff9d6a629027bd3c
Dan Cristian Bota <dan.cristian.bota@gmail.com>
2018-10-21 21:57:15 +0300

    comments controller update and destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,16 @@ class CommentsController < ApplicationController
     @comment = @post.comments.find(params[:id])
   end
 
+  def update
+    @comment = @post.comments.find(params[:id])
+    render :show if @comment.update(comments_params)
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+  end
+
   private
 
   def comments_params
