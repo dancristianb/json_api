4593264001888ba9f2b60628b21022663e8afff6
Dan Cristian Bota <dan.cristian.bota@gmail.com>
2018-10-21 18:52:37 +0300

    add show.json.jbuilder and implement posts show and create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,23 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  # rails routes this to the show.json.jbuilder view by default
+  # so three's no need to be verbose and call render :show
+  def show
+    @post = Post.find(params[:id])
+    puts @post.inspect
+  end
+
+  def create
+    @post = Post.create(post_params)
+    render :show, status: :created
+  end
+
+  private
+
+  def post_params
+    params.require(:posts).permit(:title, :description, :active)
+  end
+
 end
