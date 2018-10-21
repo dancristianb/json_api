0d812884614127222587b680a51c0e15565987bd
Dan Cristian Bota <dan.cristian.bota@gmail.com>
2018-10-21 21:44:02 +0300

    +show.json.jbuilder and comments controller create/show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,27 @@
 class CommentsController < ApplicationController
+  before_action :set_post
+
   def index
-    @comments = Comment.all
+    @comments = @post.comments
+  end
+
+  def create
+    @comment = @post.comments.new(comments_params)
+    @comment.save
+    render :show, status: :created
+  end
+
+  def show
+    @comment = @post.comments.find(params[:id])
+  end
+
+  private
+
+  def comments_params
+    params.require(:comments).permit(:author, :text)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
